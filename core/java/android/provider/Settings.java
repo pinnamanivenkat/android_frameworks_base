efba594ee09c8f5c369f53df866f99b2c2468761
maxwen <max.weninger@gmail.com>
2016-03-18 18:17:05 +0100

    [1/2] base: volume key music control
    Forward port from 4.4 times
    Big thanks to CM for some important hints how to
    implement this e.g.
    https://github.com/CyanogenMod/android_frameworks_base/commit/e9a5363f51d22e776813ae18272734dfc992947f
    
    Change-Id: Ib0839d4fb7f411d85023aa1de61716b7f78f2e97
    
    ambient display: Fix volume key music control
    
    Previously the dream service would only report whether or not it was dreaming.
    Ambient display counts as dreaming.  When dreaming, the volume inputs are
    passed back to the application which is not what we want for ambient
    display.
    
    Fix this by exposing isDozing() and allow volume keys to control music
    when we are dreaming and have moved into doze mode (aka, sleeping).
    
    Change-Id: I3b1e72b95ec8b1b63ef0219259dfee77a5d339d3
    
    * FHEdits *
    ON by default

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3936,6 +3936,12 @@ public final class Settings {
          */
         public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
 
+        /** Whether to skip music track with volume rocker 
+        /**
+         * @hide
+         */
+        public static final String VOLUME_BUTTON_MUSIC_CONTROL = "volume_button_music_control";
+
         /**
          * MediaScanner behavior on boot.
          * 0 = enabled
@@ -4107,6 +4113,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(VOLUME_KEYS_CONTROL_MEDIA_STREAM);
             PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_LOCKSCREEN);
             PRIVATE_SETTINGS.add(STATUS_BAR_QUICK_QS_PULLDOWN);
+            PRIVATE_SETTINGS.add(VOLUME_BUTTON_MUSIC_CONTROL);
         }
 
         /**

1bf6387593be1a291fae38b9b9f176f524f9caab
The-DarkBeast <pranaynarang@gmail.com>
2017-11-07 11:19:03 +0530

    Revert "GlobalActions: Set the initial status of airplane mode toggle"
    This reverts commit b758c2efc7a246e1a771e8c24cbe5a73fa445543.

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -170,9 +170,6 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
                 R.bool.config_useFixedVolume);
 
         mEmergencyAffordanceManager = new EmergencyAffordanceManager(context);
-
-        // Set the initial status of airplane mode toggle
-        mAirplaneState = getUpdatedAirplaneToggleState();
     }
 
     /**
